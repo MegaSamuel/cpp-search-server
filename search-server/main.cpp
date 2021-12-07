f90bc10980f6295e46430f61348f258a60ab3b7a
Sergei Medvedev <cepaleme@gmail.com>
2021-12-07 16:31:16 +0300

    main : answer to a riddle

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,34 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
 // Напишите ответ здесь:
 
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch = '3';
+    int count = 0;
+    string str;
+
+    for(int i = 1; i <= 1000; i++)
+    {
+        str = to_string(i);
+        for(int i = 0; i < str.size(); i++)
+        {
+            if(ch == str[i])
+            {
+                count++;
+                break;
+            }
+        }
+    }
+
+    cout << count;
+}
+*/
+
+// 271 число содержит как минимум одну тройку
+
 // Закомитьте изменения и отправьте их в свой репозиторий.
