75c9896ccbb2d95fc9f66d486a1053bb934e4961
Sergei Medvedev <cepaleme@gmail.com>
2022-03-30 13:45:57 +0300

    add ProcessQueriesJoined

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -272,7 +272,7 @@ int main() {
 }
 #endif
 
-#if 1
+#if 0
 {
     SearchServer search_server("and with"s);
 
@@ -299,6 +299,34 @@ int main() {
         const auto& documents : ProcessQueries(search_server, queries)
     ) {
         cout << documents.size() << " documents for query ["s << queries[id++] << "]"s << endl;
+    }
+}
+#endif
+
+#if 1
+{
+    SearchServer search_server("and with"s);
+
+    int id = 0;
+    for (
+        const string& text : {
+            "funny pet and nasty rat"s,
+            "funny pet with curly hair"s,
+            "funny pet and not very nasty rat"s,
+            "pet with rat and rat and rat"s,
+            "nasty rat with curly hair"s,
+        }
+    ) {
+        search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
+    }
+
+    const vector<string> queries = {
+        "nasty rat -not"s,
+        "not very funny nasty pet"s,
+        "curly hair"s
+    };
+    for (const Document& document : ProcessQueriesJoined(search_server, queries)) {
+        cout << "Document "s << document.id << " matched with relevance "s << document.relevance << endl;
     }
 }
 #endif
--- search-server/process_queries.cpp
+++ search-server/process_queries.cpp
@@ -23,3 +23,20 @@ std::vector<std::vector<Document>> ProcessQueries(const SearchServer& search_ser
 
     return documents_lists;
 }
+
+std::list<Document> ProcessQueriesJoined(const SearchServer& search_server,
+                                           const std::vector<std::string>& queries) {
+    std::vector<std::vector<Document>> documents_lists(queries.size());
+
+    documents_lists = ProcessQueries(search_server, queries);
+
+    std::list<Document> documents;
+
+    for(auto it = documents_lists.begin(); it != documents_lists.end(); it++) {
+        for(auto iit = it->begin(); iit != it->end(); iit++) {
+            documents.push_back(std::move(*iit));
+        }
+    }
+
+    return documents;
+}
